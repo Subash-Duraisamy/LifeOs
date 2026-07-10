de46e89c657e34bed35206aa4c2405df34c1cebd
SUBASHOFFI <redmisubashias@gmail.com>
2026-07-10 10:49:08 +0530

    Fix Netlify redirects

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,16 +1,14 @@
 import { initializeApp } from "firebase/app";
-
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyARnugZtC5kencbiq6w6oVJJEBukm923F0",
-  authDomain: "lifeos-84289.firebaseapp.com",
-  projectId: "lifeos-84289",
-  storageBucket: "lifeos-84289.firebasestorage.app",
-  messagingSenderId: "203120174698",
-  appId: "1:203120174698:web:4a8dd6f7eec6ade7ffcc13",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
